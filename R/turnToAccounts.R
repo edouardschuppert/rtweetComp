8e668843bff7dc64984dca9b25fdb90b0766f20a
edouardschuppert <edouard.schuppert@gmail.com>
2018-11-30 12:28:26 +0100

    turnToAccount : add url_account col

--- R/turnToAccounts.R
+++ R/turnToAccounts.R
@@ -14,7 +14,7 @@ turnToAccount <- function(df) {
   df %>%
     mutate(url_account = paste0("https://twitter.com/", .data$screen_name)) %>%
     select(.data$user_id, .data$screen_name, .data$followers_count, .data$friends_count, .data$listed_count, .data$statuses_count, .data$favourites_count,
-           .data$description, .data$url, .data$protected, .data$name, .data$location, .data$account_created_at, .data$verified, .data$profile_url,
+           .data$description, .data$url, .data$url_account, .data$protected, .data$name, .data$location, .data$account_created_at, .data$verified, .data$profile_url,
            .data$profile_expanded_url, .data$account_lang, .data$profile_banner_url, .data$profile_background_url, .data$profile_image_url)
 
 }
